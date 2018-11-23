a9f0c76e4b6f17bd71a26c3eb5b4aa08e08b3ce2
Fabio Colella <fcole90@gmail.com>
2018-11-23 18:05:18 +0200

    Added setup file

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+setup(
+    name="demotivational_policy_descent",
+    packages=["demotivational_policy_descent"],
+    version="0.0.1.dev1",
+    description="Reinforcement Learning",
+    author="Fabio Colella",  # packager 
+    author_email="fabio.colella@aalto.fi",
+    url="https://github.com/fcole90/demotivational-policy-descent.git",
+    download_url="https://github.com/fcole90/demotivational-policy-descent.git",
+    keywords=["reinforcement", "learning"],
+    license="MIT License",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research"
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        ],
+    long_description="""\
+Final project for the 2018 edition of the course of Reinforcement Learning
+at Aalto University, Finland.
+""",
+    install_requires=['gym', 'numpy', 'Pygame', 'matplotlib'],
+    python_requires='>=3.4'
+)
